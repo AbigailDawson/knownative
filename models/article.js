a8192bd71e1ad025ae46b1c3dc8fc636cf80a0be
Abigail Dawson <abigaildawson.dev@gmail.com>
2023-12-16 20:29:13 -0500

    Define article and comment schemas

--- models/article.js
+++ models/article.js
@@ -1,6 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const articleSchema = new Schema({
-    
+const commentSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    userName: String,
+    userAvatar: String,
+    content: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
 })
+
+const articleSchema = new Schema({
+    title: String,
+    source: String,
+    category: String,
+    content: String,
+    public: Boolean,
+    user: [ObjectId],
+    cards: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Card'
+    }],
+    comments: [commentSchema]
+}, {
+    timestamps: true
+})
+
+const Article = mongoose.model('Article', articleSchema)
