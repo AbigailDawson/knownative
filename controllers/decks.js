e44a3ed27a9e0f2a4fc7b765c6d5615882abb666
Abigail Dawson <abigaildawson.dev@gmail.com>
2023-12-19 19:27:09 -0500

    Add decks controller file, create function for listing all publicly shared decks

---
+++ controllers/decks.js
@@ -0,0 +1,114 @@
+const Deck = require('../models/deck');
+
+module.exports = {
+    all,
+    // mine,
+    // new: newArticle,
+    // create,
+    // show,
+    // edit,
+    // update,
+    // delete: deleteArticle,
+    // save
+}
+
+async function all(req, res) {
+    const decks = await Deck.find({ 'public' : true });
+    res.render('decks/all', { 
+        title: 'All Decks', 
+        decks 
+    })
+}
+
+// async function mine(req, res) {
+//     const articles = await Article.find({ 
+//         $or: [
+//             { userCreating: req.user._id },
+//             { userSaving: req.user._id }
+//         ]
+//      });
+//     res.render('articles/index', { 
+//         title: 'My Articles', 
+//         articles 
+//     })
+// }
+
+// async function newArticle(req, res) {
+//     const validCategories = await Article.schema.path('category').enumValues;
+//     res.render('articles/new', {
+//         title: 'Add an Article',
+//         validCategories,
+//         errorMsg: 'Add article failed'
+//     })
+// }
+
+// async function create(req, res) {
+//     const article = new Article(req.body);
+//     article.userCreating = req.user._id;
+//     article.userName = req.user.name;
+//     article.userAvatar = req.user.avatar;
+//     try {
+//         await article.save();
+//         console.log(article)
+//         res.redirect(`/articles`);
+//     } catch (err) {
+//         console.log(err.message)
+//         res.redirect('/articles/new')
+//     }
+// }
+
+// async function show(req, res) {
+//     const article = await Article.findById(req.params.id).populate('userCreating')
+//     res.render('articles/show', {
+//         title: `${article.title}`,
+//         article
+//     })
+// }
+
+// async function edit(req, res) {
+//     const article = await Article.findOne({
+//         _id: req.params.id,
+//         userCreating: req.user._id
+//     })
+//     if (!article) return res.redirect('/articles');
+//     const validCategories = await Article.schema.path('category').enumValues;
+//     res.render('articles/edit', {
+//         title: 'Edit Details',
+//         article,
+//         validCategories
+//     })
+// }
+
+// async function update(req, res) {
+//     try {
+//         if(!req.body.public) {
+//             req.body.public = false;
+//         }
+//         const updatedArticle = await Article.findOneAndUpdate(
+//             { _id: req.params.id, userCreating: req.user._id},
+//             req.body,
+//             { new: true }
+//         );
+//         return res.redirect(`/articles/${updatedArticle._id}`)
+//     } catch (err) {
+//         console.log(err.message);
+//         return res.redirect('/articles')
+//     }
+// }
+
+// async function deleteArticle(req, res) {
+//     await Article.findOneAndDelete({
+//         _id: req.params.id,
+//         userCreating: req.user._id
+//     });
+//     res.redirect('/articles')
+// }
+
+// async function save(req, res) {
+//     const article = await Article.findById(req.params.id);
+//     console.log('article: ', article)
+//     if (article.usersSaving.id(req.user._id)) return res.redirect('/articles');
+//     article.usersSaving.push(req.user._id);
+//     await article.save();
+//     res.redirect(`articles/${article._id}`)
+// }
--- routes/decks.js
+++ routes/decks.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 const decksCtrl = require('../controllers/decks')
 
-router.get('/all', decksCtrl.allDecks)
+router.get('/all', decksCtrl.decksCtrl)
 // router.get('/', articlesCtrl.myArticles)
+// router.get('/new', articlesCtrl.newArticle)
+// router.get('/:id', articlesCtrl.showArticle)
+// router.get('/:id/edit', articlesCtrl.editArticle)
+// router.post('/', articlesCtrl.createArticle)
+// router.put('/:id', articlesCtrl.updateArticle)
+// router.delete('/:id', articlesCtrl.deleteArticle)
+// router.post('/:id/save', articlesCtrl.saveArticle);
 
 module.exports = router;
