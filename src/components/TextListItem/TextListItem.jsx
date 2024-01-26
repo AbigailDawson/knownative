6c04036ee6fa9a8431b6c7d71c574d85dc1307e9
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-01-26 14:41:21 -0500

    Adjust styling on Dashboard page for easier navigation during development

--- src/components/TextListItem/TextListItem.jsx
+++ src/components/TextListItem/TextListItem.jsx
@@ -6,8 +6,6 @@ export default function TextListItem({ title, source, content, id }) {
   return (
     <div className="TextListItem">
       <h1>{title}</h1>
-      <h3>Source: {source}</h3>
-      <p>{content}</p>
       <Link to={`/read/${id}`}>Read</Link>
     </div>
   )
