308397c6d8fcd9a7433694de3b4e26c0a937e57c
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-01-26 21:52:42 -0500

    Adjust styling on sidebar for readability during development

--- src/components/SavedWord/SavedWord.jsx
+++ src/components/SavedWord/SavedWord.jsx
@@ -1,9 +1,9 @@
+import './SavedWord.css'
+
 export default function SavedWord({ word }) {
   return (
     <div className="SavedWord">
-      <p>{ word.traditional }</p>
-      <p>{ word.pinyin }</p>
-      <p>{ word.meaning }</p>
+      <p>{ word.traditional } &nbsp; { word.pinyin } </p> <p>{ word.meaning }</p>
     </div>
   )
 }
