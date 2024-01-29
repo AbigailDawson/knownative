180c6756b29a5dc4670b0e01d60b93a4c65b486f
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-01-29 14:28:29 -0500

    Create routes for words data

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(require('./config/checkToken'))
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/texts', require('./routes/api/texts'))
+app.use('/api/words', require('./routes/api/words'))
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
