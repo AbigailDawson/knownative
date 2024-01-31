3d1cf4f54d58b2fec7005de42658bd34d3d93299
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-01-31 13:35:56 -0500

    Move GPT simplify text content to modal

--- src/components/ReadText/ReadText.jsx
+++ src/components/ReadText/ReadText.jsx
@@ -1,9 +1,12 @@
 import './ReadText.css'
 import { useState } from 'react'
 import * as textsAPI from '../../utilities/texts-api'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+import { FaRegWindowClose } from "react-icons/fa";
 
 export default function ReadText({ text }) {
 
+  const [open, setOpen] = useState(false)
   const [simplifiedText, setSimplifiedText] = useState(null)
   const [loading, setLoading] = useState(false)
 
@@ -20,29 +23,78 @@ export default function ReadText({ text }) {
     }
   }
 
+  function handleOpen() {
+    setOpen(true)
+  }
+
+  function handleClose() {
+    setOpen(false)
+  }
+
+
   return (
     <div className="ReadText">
 
       <h1 className="zh">{text.title}</h1>
       <h3>Source: {text.source}</h3>
 
-      {loading ? (
-        <span>Loading... (may take several seconds)</span>
-      ) : (
-        !simplifiedText && (
-          <span>
-            Read an easier version of this text --
-            <button onClick={handleSimplifyClick}> Click Me! </button>
-          </span>
-        )
-      )}
+      <span>Read an easier version of this text -- <button onClick={handleOpen}>Open</button></span>
+      <Dialog
+          open={open}
+          onClose={handleClose}   
+          PaperComponent={({ children }) => (
+            <div style={{ 
+              width: '60vmin', 
+              height: '50vmin',
+              backgroundColor: 'white',
+              color: 'var(--drk-txt)',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              padding: '1vmin',
+              borderRadius: '2vmin'
+              }}>
+              {children}
+            </div>
+          )}
+        >
+        <DialogActions style={{ 
+          alignSelf: 'flex-end',
+          padding: '0'
+          }}>
+          <Button
+          onClick={handleClose}>
+            <FaRegWindowClose className="close-icon"/>
+          </Button>
+        </DialogActions>
 
-      { simplifiedText && (
-        <div>
-          <p className="simplified-text zh">{simplifiedText}</p>
-          <p className="disclaimer">Disclaimer: This text has been generated using artificial intelligence. While the model strives to produce accurate and coherent content, it may occasionally contain inaccuracies, grammatical errors, or unintended meaning. The AI model does not guarantee perfection, and the user is encouraged to exercise their judgment when interpreting the output.</p>
-        </div>
-      )}
+        <DialogContent>
+          <DialogTitle>Feeling Stuck?</DialogTitle>
+          <DialogContentText>
+            <h3> Click to generate an easier version of this text. </h3>
+            
+
+            {loading ? (
+              <span>Loading... (may take several seconds)</span>
+            ) : (
+              !simplifiedText && (
+                  <button onClick={handleSimplifyClick}> Generate </button>
+              )
+            )}
+            
+            { simplifiedText && (
+              <div>
+                <p className="simplified-text zh">{simplifiedText}</p>
+              </div>
+            )}
+
+            <p className="disclaimer">Disclaimer: This text is generated using artificial intelligence. While the model strives to produce accurate and coherent content, it may occasionally contain inaccuracies, grammatical errors, or unintended meaning. The AI model does not guarantee perfection, and the user is encouraged to exercise their judgment when interpreting the output.</p>
+          </DialogContentText>
+        </DialogContent>
+
+      </Dialog>
+
+      
 
         <div className="read-text-block">
           <p className="zh">{text.content}</p>
