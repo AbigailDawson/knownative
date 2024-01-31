9271f3eb628cc9edd79e666ca7d912e729c63f82
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-01-31 15:46:11 -0500

    Adjust styling on ReadText component

--- src/components/ReadText/ReadText.jsx
+++ src/components/ReadText/ReadText.jsx
@@ -47,77 +47,77 @@ export default function ReadText({ text }) {
   return (
     <div className="ReadText">
 
-      <h1 className="zh">{text.title}</h1>
-      <h3>Source: {text.source}</h3>
+      <div className="top">
+        <span>Read an easier version of this text -- <button onClick={handleOpen}>Open</button></span>
+        <Dialog
+            open={open}
+            onClose={handleClose}   
+            PaperComponent={({ children }) => (
+              <div style={{ 
+                width: '60vmin', 
+                height: '50vmin',
+                backgroundColor: 'white',
+                color: 'var(--drk-txt)',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                padding: '1vmin',
+                borderRadius: '2vmin'
+                }}>
+                {children}
+              </div>
+            )}
+          >
+          <DialogActions style={{ 
+            alignSelf: 'flex-end',
+            padding: '0'
+            }}>
+            <Button
+            onClick={handleClose}>
+              <FaRegWindowClose className="close-icon"/>
+            </Button>
+          </DialogActions>
 
-      <span>Read an easier version of this text -- <button onClick={handleOpen}>Open</button></span>
-      <Dialog
-          open={open}
-          onClose={handleClose}   
-          PaperComponent={({ children }) => (
-            <div style={{ 
-              width: '60vmin', 
-              height: '50vmin',
-              backgroundColor: 'white',
-              color: 'var(--drk-txt)',
+          <DialogContent>
+            <DialogTitle style={{ 
+              textAlign: 'center'
+              }}
+              >Feeling Stuck?</DialogTitle>
+            <DialogContentText
+            style={{ 
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
-              padding: '1vmin',
-              borderRadius: '2vmin'
+              justifyContent: 'center',
               }}>
-              {children}
-            </div>
-          )}
-        >
-        <DialogActions style={{ 
-          alignSelf: 'flex-end',
-          padding: '0'
-          }}>
-          <Button
-          onClick={handleClose}>
-            <FaRegWindowClose className="close-icon"/>
-          </Button>
-        </DialogActions>
+                <h3> Click to generate an easier version of this text. </h3>
 
-        <DialogContent>
-          <DialogTitle style={{ 
-            textAlign: 'center'
-            }}
-            >Feeling Stuck?</DialogTitle>
-          <DialogContentText
-           style={{ 
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            }}>
-              <h3> Click to generate an easier version of this text. </h3>
+                  {!simplifiedText && !loading && <button className="generate-btn" onClick={handleSimplifyClick}> Generate </button>}
 
-                {!simplifiedText && !loading && <button className="generate-btn" onClick={handleSimplifyClick}> Generate </button>}
+                  <Suspense fallback={<h2>Loading...</h2>}>
+                    {loading ? (
+                      <div class="loader"></div>
+                      ) : (
+                        <>
+                          {simplifiedText && <SimplifiedText simplifiedText={simplifiedText} saveSimplifiedText={saveSimplifiedText} />}
+                        </>
+                        
+                      )}
+                  </Suspense>
+                <p className="disclaimer">Disclaimer: This text is generated using artificial intelligence. While the model strives to produce accurate and coherent content, it may occasionally contain inaccuracies, grammatical errors, or unintended meaning. The AI model does not guarantee perfection, and the user is encouraged to exercise their judgment when interpreting the output.</p>
+              
+            </DialogContentText>
+          </DialogContent>
 
-                <Suspense fallback={<h2>Loading...</h2>}>
-                  {loading ? (
-                    <div class="loader"></div>
-                    ) : (
-                      <>
-                        {simplifiedText && <SimplifiedText simplifiedText={simplifiedText} saveSimplifiedText={saveSimplifiedText} />}
-                      </>
-                      
-                    )}
-                </Suspense>
-              <p className="disclaimer">Disclaimer: This text is generated using artificial intelligence. While the model strives to produce accurate and coherent content, it may occasionally contain inaccuracies, grammatical errors, or unintended meaning. The AI model does not guarantee perfection, and the user is encouraged to exercise their judgment when interpreting the output.</p>
-            
-          </DialogContentText>
-        </DialogContent>
+        </Dialog>
+      </div>
 
-      </Dialog>
-
-      
-
+      <div className="bottom">
         <div className="read-text-block">
           <p className="zh">{text.content}</p>
         </div>
+      </div>
+      
     </div>
   )
 }
