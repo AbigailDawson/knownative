f33fed47275f92c0e384342f6ea09934582adcd7
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-02-01 09:58:49 -0500

    Remove console logs from send-request function

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,7 +1,6 @@
 import { getToken } from './users-service' // this is the function that retrieves the token from localStorage if there is one (and if it's not expired) -- will return either a valid token or null
 
 export default async function sendRequest(url, method = 'GET', payload = null) {
-  console.log('request sent')
   // Fetch accepts an options object as the 2nd argument used to include a data payload, set headers, etc. method and payload are default parameters -- if we don't pass something in when we call this function, these are the default values that will be sent for method and payload
   const options = { method }; // first check if there is some other method in the request
   if (payload) { // check for a payload, if we have a payload we should set it up as follows:
@@ -20,7 +19,6 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
 
   const res = await fetch(url, options) // make the fetch call
   // res.ok will be false if the status code set to 4xx in the controller action
-  console.log(res)
   if (res.ok) return res.json() // if it's all good, we send back the json object
   throw new Error('Bad Request')
 }
