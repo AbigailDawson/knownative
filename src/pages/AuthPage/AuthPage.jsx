5ebdca0234cdc0b1fe190c9ef2ad576945aa0109
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-02-03 09:08:12 -0500

    Add template for hero on landing page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,13 +1,56 @@
-// import SignUpForm from '../../components/SignUpForm/SignUpForm'
+import './AuthPage.css'
+import { useState } from 'react'
 import SignUpFormFunc from '../../components/SignUpFormFunc/SignUpFormFunc'
 import LogInForm from '../../components/LogInForm/LogInForm'
 
 export default function AuthPage({setUser}) {
+
+  const [showLogin, setShowLogin] = useState(false)
+  const [showSignup, setShowSignup] = useState(false)
+
+  function handleGoBack() {
+    setShowLogin(false)
+    setShowSignup(false)
+  }
+
   return (
-    <main>
-      <h1>AuthPage</h1>
-      <SignUpFormFunc setUser={setUser} />
-      <LogInForm setUser={setUser} />
+    <main className="landing-page">
+
+      <section className="hero">
+        <div className="hero-left">
+          <img src="" alt="Hero Image" />
+        </div>
+        <div className="hero-right">
+          
+          {!showLogin && !showSignup ? (
+            <>
+              <h1>Unlock the Power of Native Texts</h1>
+              <h4>Ditch the textbooks and generic courses! With KnowNative, you have the freedom to tailor your own learning experience by reading texts from anywhere that piques your interest. Immerse yourself in real-life language usage and discover the beauty of traditional Chinese through the written words of native speakers.</h4>
+              <div className="auth-btns">
+                <button onClick={() => setShowLogin(!showLogin)}>Log In</button>
+                <button onClick={() => setShowSignup(!showSignup)}>Sign Up</button>
+              </div>
+            </>
+          ) : (
+            <>
+            { showLogin && (
+              <>
+                <h1>Log In</h1>
+                <button onClick={handleGoBack}>Back</button>
+                <LogInForm setUser={setUser} />
+              </>
+            )}
+            { showSignup && (
+              <>
+                <h1>Sign Up</h1>
+                <button onClick={handleGoBack}>Back</button>
+                <SignUpFormFunc setUser={setUser} />
+              </>
+            )}
+          </>
+          )}
+        </div>
+      </section>
     </main>
   )
 }
