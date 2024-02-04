3f452f4643ab2439f9197cd022d8ab303fdf09b2
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-02-03 23:47:34 -0500

    Update navbar logo and styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,15 +6,15 @@ export default function NavBar({ user, setUser }) {
   const navigate = useNavigate();
 
   function handleLogOut() {
-    usersService.logOut() // userService is imported as an alias. this logOut() will remove the token (see users-service module)
-    setUser(null) // don't forget to bring in setUser as a prop in the function params
+    usersService.logOut() 
+    setUser(null)
     navigate('/')
   }
 
   return (
     <nav>
       <div className="nav-left">
-        <h1>KnowNative</h1>
+        <img src="/images/knlogo2.png" alt="KnowNative Logo Image" style={{height: '11vmin'}} />
       </div>
       <div className="nav-right">
         <Link to='/dashboard' className="nav-link">Dashboard</Link>
