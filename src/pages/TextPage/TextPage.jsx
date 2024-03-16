afca692362a7ffa4923014e20db563b1d4990368
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-03-16 12:27:23 -0400

    Minor bug fix

--- src/pages/TextPage/TextPage.jsx
+++ src/pages/TextPage/TextPage.jsx
@@ -69,7 +69,7 @@ export default function TextPage({ getText, updateText }) {
       }
     }
     getSavedWords()
-  }, [text])
+  })
 
   async function saveEasierText(easierText) {
     const updatedText = await textsAPI.saveEasierText(easierText, text._id);
