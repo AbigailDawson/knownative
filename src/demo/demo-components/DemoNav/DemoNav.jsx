eac80a6ec3c53da635c6d84f50b185e3346c93fd
Abigail Dawson <145867967+AbigailDawson@users.noreply.github.com>
2024-05-27 16:09:42 -0400

    Add placeholder icons and logo to far left navbar
    Signed-off-by: Abigail Dawson <145867967+AbigailDawson@users.noreply.github.com>

--- src/demo/demo-components/DemoNav/DemoNav.jsx
+++ src/demo/demo-components/DemoNav/DemoNav.jsx
@@ -1,9 +1,24 @@
 import './DemoNav.css'
+import { TbCardsFilled } from "react-icons/tb";
+import { MdQuiz } from "react-icons/md";
+import { FaArrowsRotate } from "react-icons/fa6";
+import { ImExit } from "react-icons/im";
+import { FaRegQuestionCircle } from "react-icons/fa";
 
 export default function DemoNav() {
 
   return (
     <>
+      <div className='top-icons'>
+        <img src="/images/placeholder-logo.png" alt="logo" className="placeholder-logo" />
+        <TbCardsFilled className="side-nav-icon-top" />
+        <MdQuiz className="side-nav-icon-top" />
+        <FaArrowsRotate className="side-nav-icon-top" />
+      </div>
+      <div className="bottom-icons">
+        <FaRegQuestionCircle className="side-nav-icon-bottom" />
+        <ImExit className="side-nav-icon-bottom" />
+      </div>
     </>
   )
 }
