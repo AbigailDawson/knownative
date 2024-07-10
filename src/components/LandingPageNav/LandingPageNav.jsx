45c18cefc75f6ea40d7857cd07b51bd4c3aaec1c
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-07-10 16:28:41 -0400

    Add landing page navbar component
    Signed-off-by: Abigail Dawson <abigaildawson.dev@gmail.com>

---
+++ src/components/LandingPageNav/LandingPageNav.jsx
@@ -0,0 +1,17 @@
+export default function LandingPageNav() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light mx-4 py-3">
+      <div class="container px-4 px-lg-5">
+          <a class="navbar-brand" href="#page-top">KnowNative</a>
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto my-2 my-lg-0">
+              <li class="nav-item"><a class="nav-link" href="/">About</a></li>
+              <li class="nav-item"><a class="nav-link" href="/">Contact</a></li>
+              <li class="nav-item"><a class="nav-link" href="/">GitHub</a></li>
+            </ul>
+          </div>
+      </div>
+  </nav>
+  )
+}
