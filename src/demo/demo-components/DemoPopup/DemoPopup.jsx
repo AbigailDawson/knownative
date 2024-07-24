ef2bb36c093837054ff6f2a96caac49ef392ec82
Mel Boyajian <MelBoyajian@MelBoyaiansiMac.attlocal.net>
2024-07-23 20:07:02 -0700

    changed 'save' button to '+', changed close button to 'x', added pointer cursor to study text, added darkened background on mouseover for study text, changed color of popup to lt green, merged conflict with package-lock.json

--- src/demo/demo-components/DemoPopup/DemoPopup.jsx
+++ src/demo/demo-components/DemoPopup/DemoPopup.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react';
 import './DemoPopup.css'
 
 export default function Popup({ word, popupPosition, saveWord, textId, onClose}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(true);
+    const handleOutsideClick = (event) => {
+      if (!event.target.closest('.Popup') && isOpen) {
+        onClose();
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [onClose]);
 
   let pinyin = ''
   let meaning = ''
@@ -15,26 +31,33 @@ export default function Popup({ word, popupPosition, saveWord, textId, onClose})
     meaning = ''
   }
 
-  // function handleSaveClick(word) {
-  //   saveWord(word, textId)
-  // }
-
   function handleSaveClick(word) {
     saveWord(word)
   }
 
   return (
     <div className="Popup" style={{ 
-      left: `${popupPosition[0] + 30}px`,
-      top: `${popupPosition[1] - 50}px`,
+      left: `${popupPosition[0] + 0}px`,
+      top: `${popupPosition[1] - 75}px`,
       }}>
         <div className="popup-content">
           <p>{pinyin}</p>
           <p>{meaning}</p>
         </div>
 
-        <button onClick={() => handleSaveClick(word)}> Save </button> &nbsp; 
-        <button onClick={() => onClose()}> Close </button>
+        <button className="save-button" onClick={() => handleSaveClick(word)}> + </button> &nbsp; 
+        <button className="close-btn" onClick={() => {
+          onClose();
+          setIsOpen(false);
+        }}> x </button>
     </div>
   )
 }
+
+
+
+
+
+ // function handleSaveClick(word) {
+  //   saveWord(word, textId)
+  // }
