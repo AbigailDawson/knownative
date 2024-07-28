b13e5a1ce45aef3b307a11e4549f3aa4d6dacfae
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-07-28 13:42:18 -0500

    styling now uses root color vars, and custom styling instead of using important. React tooltips css file no longer in index page due to using custom styling

--- src/demo/demo-components/DemoNav/DemoNav.jsx
+++ src/demo/demo-components/DemoNav/DemoNav.jsx
@@ -14,52 +14,93 @@ export default function DemoNav({ expandSidebar }) {
 
   return (
     <>
-      <div className='top-icons'>
-        <img src="/images/placeholder-logo.png" alt="logo" className="placeholder-logo" />
-        <TbCardsFilled 
-          className="side-nav-icon-top" 
-          onClick={handleCardsClick} 
-          data-tooltip-id="savedwords-tooltip" 
-          data-tooltip-content="Saved Words" 
-          data-tooltip-place="right" 
+      {/* Sidebar Icons */}
+      <div className="top-icons">
+        <img
+          src="/images/placeholder-logo.png"
+          alt="logo"
+          className="placeholder-logo"
         />
-        <Tooltip id="savedwords-tooltip" className="icon-tool-tip"
-        border="1px solid #526662" />
-        <MdQuiz 
-          className="side-nav-icon-top" 
-          data-tooltip-id="flashcards-tooltip" 
-          data-tooltip-content="Flashcard Quiz" 
-          data-tooltip-place="right" 
-        />
-        <Tooltip id="flashcards-tooltip" className="icon-tool-tip"
-        border="1px solid #526662" />
-        <FaArrowsRotate 
-          className="side-nav-icon-top" 
-          data-tooltip-id="changetext-tooltip" 
-          data-tooltip-content="Change Text" 
-          data-tooltip-place="right" 
-        />
-        <Tooltip id="changetext-tooltip" className="icon-tool-tip" 
-        border="1px solid #526662" />
+        {/* Saved Words Icon*/}
+        <div className="icontip-container">
+          <TbCardsFilled
+            className="side-nav-icon-top"
+            onClick={handleCardsClick}
+            data-tooltip-id="savedwords-tooltip"
+            data-tooltip-content="Saved Words"
+            data-tooltip-place="right"
+          />
+          <Tooltip
+            id="savedwords-tooltip"
+            className="icontip-message"
+            border="1px solid var(--medium)"
+            disableStyleInjection="true"
+          />
+        </div>
+        {/* Quiz Icon */}
+        <div className="icontip-container">
+          <MdQuiz
+            className="side-nav-icon-top"
+            data-tooltip-id="flashcards-tooltip"
+            data-tooltip-content="Flashcard Quiz"
+            data-tooltip-place="right"
+          />
+          <Tooltip
+            id="flashcards-tooltip"
+            className="icontip-message"
+            border="1px solid var(--medium)"
+            disableStyleInjection="true"
+          />
+        </div>
+        {/* Change Text Icon */}
+        <div className="icontip-container">
+          <FaArrowsRotate
+            className="side-nav-icon-top"
+            data-tooltip-id="changetext-tooltip"
+            data-tooltip-content="Change Text"
+            data-tooltip-place="right"
+            disableStyleInjection="true"
+          />
+          <Tooltip
+            id="changetext-tooltip"
+            className="icontip-message"
+            border="1px solid var(--medium)"
+            disableStyleInjection="true"
+          />
+        </div>
       </div>
       <div className="bottom-icons">
-        <FaRegQuestionCircle  
-          className="side-nav-icon-bottom" 
-          data-tooltip-id="support-tooltip" 
-          data-tooltip-content="Support" 
-          data-tooltip-place="right"
-        />
-        <Tooltip id="support-tooltip" className="icon-tool-tip" 
-        border="1px solid #526662" />
-        <ImExit 
-          className="side-nav-icon-bottom" 
-          data-tooltip-id="exit-tooltip" 
-          data-tooltip-content="Exit Demo" 
-          data-tooltip-place="right" 
-        />
-        <Tooltip id="exit-tooltip" className="icon-tool-tip" 
-        border="1px solid #526662"/>
+        {/* Support Icon */}
+        <div className="icontip-container">
+          <FaRegQuestionCircle
+            className="side-nav-icon-bottom"
+            data-tooltip-id="support-tooltip"
+            data-tooltip-content="Support"
+            data-tooltip-place="right"
+          />
+          <Tooltip
+            id="support-tooltip"
+            className="icontip-message"
+            border="1px solid var(--medium)"
+            disableStyleInjection="true"
+          />
+        </div>
+        {/* Exit Icon */}
+        <div className="icontip-container">
+          <ImExit
+            className="side-nav-icon-bottom"
+            data-tooltip-id="exit-tooltip"
+            data-tooltip-content="Exit Demo"
+            data-tooltip-place="right"
+          />
+          <Tooltip
+            id="exit-tooltip"
+            className="icontip-message"
+            border="1px solid var(--medium)"
+            disableStyleInjection="true"
+          />
+        </div>
       </div>
     </>
-  )
+  );
 }
