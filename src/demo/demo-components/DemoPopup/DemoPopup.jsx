e8673750431aebda7426e99e942ba584bdac9b2f
Mel Boyajian <MelBoyajian@MelBoyaiansiMac.attlocal.net>
2024-07-31 13:51:18 -0700

    added bold glyph to popup, changed box height depending on how many lines are in the 'meaning', added a MUI icon & used as an SVG to add a stroke to match the popup box shadow, enabled popup to open on click of consecutive words

--- src/demo/demo-components/DemoPopup/DemoPopup.jsx
+++ src/demo/demo-components/DemoPopup/DemoPopup.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import './DemoPopup.css'
 
 export default function Popup({ word, popupPosition, saveWord, textId, onClose}) {
   const [isOpen, setIsOpen] = useState(false);
+  const [popupHeight, setPopupHeight] = useState('15%'); 
 
   useEffect(() => {
-    setIsOpen(true);
     const handleOutsideClick = (event) => {
       if (!event.target.closest('.Popup') && isOpen) {
         onClose();
@@ -15,6 +16,7 @@ export default function Popup({ word, popupPosition, saveWord, textId, onClose})
     document.addEventListener('click', handleOutsideClick);
     return () => {
       document.removeEventListener('click', handleOutsideClick);
+      setIsOpen(false); 
     };
   }, [onClose]);
 
@@ -35,29 +37,46 @@ export default function Popup({ word, popupPosition, saveWord, textId, onClose})
     saveWord(word)
   }
 
-  return (
-    <div className="Popup" style={{ 
-      left: `${popupPosition[0] + 0}px`,
-      top: `${popupPosition[1] - 75}px`,
-      }}>
-        <div className="popup-content">
-          <p>{pinyin}</p>
-          <p>{meaning}</p>
-        </div>
+  useEffect(() => {
+    const popupMeaning = document.querySelector('.popup-meaning');
+    const html = popupMeaning.innerHTML;
+    const lines = html.split('<br>').length; // count the number of lines by splitting on <br> elements
+    if (lines > 1) {
+      setPopupHeight('17%'); // update popup height to 17% if there are more than 1 line
+    } else {
+      setPopupHeight('15%'); // keep popup height to 15% if there is only 1 line
+    }
+  }, [meaning]);
 
-        <button className="save-button" onClick={() => handleSaveClick(word)}> + </button> &nbsp; 
-        <button className="close-btn" onClick={() => {
-          onClose();
-          setIsOpen(false);
-        }}> x </button>
+  return (
+      <div
+        className="Popup"
+        style={{
+          left: `${popupPosition[0] + 105}px`,
+          top: popupHeight === '15%'? `${popupPosition[1] - 10}px` : `${popupPosition[1] - 14}px`,
+          height: popupHeight,
+        }}
+      >
+      <div className="popup-content">
+        <p className="popup-pinyin">{pinyin}</p>
+        <p className="popup-glyphs">{word.text}</p>
+        <p className="popup-meaning">
+          {meaning.length > 25 ? (
+            <span>
+              {meaning.substring(0, meaning.lastIndexOf(' ', 20))}<br />
+              {meaning.substring(meaning.lastIndexOf(' ', 20) + 1)}
+            </span>
+          ) : (
+            meaning
+          )}
+        </p>
+      </div>
+      <ArrowDropDownIcon className="play-arrow-icon" />
+      <button className="save-button" onClick={() => handleSaveClick(word)}> + </button> &nbsp; 
+      <button className="close-btn" onClick={() => {
+        onClose();
+        setIsOpen(false);
+      }}> x </button>
     </div>
   )
 }
-
-
-
-
-
- // function handleSaveClick(word) {
-  //   saveWord(word, textId)
-  // }
