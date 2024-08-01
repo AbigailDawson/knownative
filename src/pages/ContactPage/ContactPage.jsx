84eb08b27745e8d454375f68de73d7e2e5234171
seannamercedes <rajbansiseanna@gmail.com>
2024-07-31 21:44:05 -0400

    cleaned up code on ContactPage

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import LandingPageNav from '../../components/LandingPageNav/LandingPageNav';
 import LandingPageFooter from '../../components/LandingPageFooter/LandingPageFooter';
 
