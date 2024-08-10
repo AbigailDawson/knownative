7e1b99de06ceb5c6bb447eccae1bc28ff5d39480
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-08-10 16:32:25 -0400

    Fix top left home link to navigate back home when clicked
    Signed-off-by: Abigail Dawson <abigaildawson.dev@gmail.com>

--- src/components/LandingPageNav/LandingPageNav.jsx
+++ src/components/LandingPageNav/LandingPageNav.jsx
@@ -3,7 +3,7 @@ export default function LandingPageNav() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light mx-4 py-3">
       <div class="container px-4 px-lg-5">
-          <a class="navbar-brand" href="#page-top">KnowNative</a>
+          <a class="navbar-brand" href="/">KnowNative</a>
           <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto my-2 my-lg-0">
