fb00f5e422575b16220bcf72e918c3855570e7a6
Nakita Strangeways <40547453+nakita-strangeways@users.noreply.github.com>
2024-08-21 14:46:07 -0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintConfigPrettier,
+  {
+    rules: {
+      'no-console': ['warn']
+    }
+  },
+  eslintConfigPrettier
 ];
