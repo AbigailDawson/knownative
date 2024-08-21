0c0568696d88f85d892bda1c5494c1d34f3c1b51
Nakita Strangeways <40547453+nakita-strangeways@users.noreply.github.com>
2024-08-20 17:18:10 -0700

    adding eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+];
