760c804a9e68b2cb105621ecc4e902fe1eebab12
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-08-23 09:46:10 -0400

    Update About page content and styling
    Signed-off-by: Abigail Dawson <abigaildawson.dev@gmail.com>

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -10,9 +10,7 @@ export default function AboutPage() {
       <section className="intro">
         <div>
           <h1 className="about-header">What is KnowNative?</h1>
-          <p className="about-body">KnowNative is a language learning web application that offers a refreshing alternative to traditional textbooks by allowing learners to gather and study their own materials in one seamless platform.</p>
-          <p className="about-body">Learners of traditional Chinese who want to enhance their reading comprehension through immersive and personalized study experiences can do so with KnowNative. It's designed for those who want to fully engage with authentic language materials to gain a deeper understanding of how native speakers naturally communicate.</p>
-          <p className="about-body">KnowNative empowers learners by offering a platform where they can collect, annotate, and study their own chosen texts, all within a single intuitive interface. Featuring dynamic annotations and automatically generated flashcards from any chosen words, KnowNative puts learners in charge of their language journey.</p>
+          <p className="about-body">KnowNative provides a refreshing alternative to traditional textbooks, helping learners of traditional Chinese enhance their reading comprehension through an immersive and personalized study experience. By engaging with authentic language materials, learners gain a deeper understanding of how native speakers naturally communicate. With KnowNative, anyone can collect, annotate, and study their chosen materials, taking charge of their own learning journey.</p>
         </div>
       </section>
       <section className="creator">
@@ -28,24 +26,19 @@ export default function AboutPage() {
         </div>
       </section>
       <section className="how-to-contribute">
-        <div>
-          <h1 className="about-header">How to contribute</h1>
-          <p className="about-body">Interested in contributing to the KnowNative project? We'd love to hear from you!</p>
-          <p className="about-body">KnowNative is a community-driven project that thrives on the ideas, inspiration and expertise from people like you. We're always open to contributions from community members skilled in software development, UX/UI design, graphic art and illustration, branding, copywriting and content creation. Anyone with an interest in linguistics and language study is welcome to participate in KnowNative's development!</p>
-          <p className="about-body">There are two ways to contribute to the KnowNative project:</p>
-          </div>
-          <div className="contribute-grid">
-            <div>
-              <h2>Contribute to the open source project</h2>
-              <p className="about-body">Software developers are welcome to contribute code to the project! Any kind of contribution is meaningful, whether it's a few lines of CSS to improve some styling or an entirely new feature.</p> 
-              <p className="about-body">To contribute to the open source project, follow the instructions on <a href="https://github.com/AbigailDawson/knownative" target="_blank" rel="noreferrer">GitHub</a> to run KnowNative locally, create your own branch and submit a pull request!</p>
-            </div>
-            <div>
-              <h2>Become a Core Contributor</h2>
-              <p className="about-body">Core Contributors are involved in the long-term creative vision for the KnowNative project. We work together as a team to plan and implement new features and improve the app experience for our users. Core Contributors commit to participating in regular team meetings and contribute to the project on a regular consistent basis.</p>
-              <p>If you're interested in becoming a Core Contributor, please reach out to Abigail on <a href="https://www.linkedin.com/in/abigaildawsondev/" target="_blank" rel="noreferrer">LinkedIn</a>.</p>
-            </div>
-          </div>
+        <h1 className="about-header">How to contribute</h1>
+        <p className="about-body">KnowNative is a community-driven project that thrives on the ideas, inspiration and expertise from our contributors. We're eager to collaborate with community members skilled in software development, UX/UI design, graphic art and illustration, branding, copywriting and content creation. If you have an interest in linguistics or language study, we invite you to join us in shaping KnowNative's development!</p>
+        <p className="about-body">There are two ways to contribute to the KnowNative project:</p>
+        <ul>
+          <li>
+            <span className="li-header">Contribute to the open source project</span>
+            Software developers are welcome to contribute code to the project! Any kind of contribution is meaningful, whether it's a few lines of CSS to improve some styling or an entirely new feature. To contribute to the open source project, follow the instructions on <a href="https://github.com/AbigailDawson/knownative" target="_blank" rel="noreferrer">GitHub</a> to run KnowNative locally, create your own branch and submit a pull request!
+          </li>
+          <li>
+            <span className="li-header">Become a Core Contributor</span>
+            Core Contributors are involved in the long-term creative vision for the KnowNative project. We work together as a team to plan and implement new features and improve the app experience for our users. Core Contributors commit to participating in regular team meetings and contribute to the project on a regular consistent basis. If you're interested in becoming a Core Contributor, please reach out to Abigail on <a href="https://www.linkedin.com/in/abigaildawsondev/" target="_blank" rel="noreferrer">LinkedIn</a>.
+          </li>
+        </ul>
       </section>
       <section className="backstory">
         <div>
