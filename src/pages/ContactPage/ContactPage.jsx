3ca9481de52f09de5f2e9def1d2e3c528e2eaa98
seannamercedes <rajbansiseanna@gmail.com>
2024-08-27 14:52:27 -0400

    made extra changes to the contact page social links by relocating them to the bottom right part of the page

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,5 +1,9 @@
 import LandingPageNav from '../../components/LandingPageNav/LandingPageNav';
 import LandingPageFooter from '../../components/LandingPageFooter/LandingPageFooter';
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function ContactPage() {
   return (
@@ -15,8 +19,8 @@ export default function ContactPage() {
               <p class="col-lg-10 fs-6">As an early access member of KnowNative, you'll be able to create an account and build your own personalized study portal, uploading any article you'd like to learn from. Your articles, vocabulary lists and notes will all be saved so you'll never have to worry about losing your progress.</p>
               <p class="col-lg-10 fs-6">Sign up to be notified when this beta version is released for a unique opportunity to be one of KnowNative's first members!</p>
             </div>
-            <div class="col-md-10 mx-auto col-lg-6 mt-5">
-              <form class="p-4 p-md-5 border rounded-3 bg-light">
+            <div class="col-md-10 mx-auto col-lg-6 mt-4">
+              <form class="p-4 p-md-5 rounded-3 form">
                 <div class="form-floating mb-3">
                   <input type="text" class="form-control" id="nameInput" placeholder="Name" />
                   <label for="nameInput">Name</label>
@@ -26,20 +30,21 @@ export default function ContactPage() {
                   <label for="emailInput">Email address</label>
                 </div>
                 <button class="w-100 btn btn-lg sign-up" type="submit">Sign up</button>
-                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center my-4">
-                  <a href="https://www.linkedin.com/in/abigaildawsondev/" className="btn btn-sm px-3 social-links">LinkedIn</a>
-                  <a href="https://github.com/AbigailDawson/knownative" target="_blank" rel="noopener noreferrer" className="btn btn-sm px-3 social-links">GitHub</a>
-                  <a href="mailto:abigaildawson.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="btn btn-sm px-3 social-links">Email</a>
-                  <a href="https://x.com/abigailddev" target="_blank" rel="noopener noreferrer" className="btn btn-sm px-3 social-links">X</a>
-                </div>
                 <hr class="my-3" />
                 <small class="text-muted">KnowNative will never send you spam. Unsubscribe at any time by emailing hello@knownative.com.</small>
               </form>
             </div>
+              <div className="d-grid gap-2 d-sm-flex justify-content-end mt-2 media-links">
+                <a href="https://www.linkedin.com/in/abigaildawsondev/" className="btn btn-sm px-2 social-links"><FaLinkedin /></a>
+                <a href="https://github.com/AbigailDawson/knownative" target="_blank" rel="noopener noreferrer" className="btn btn-sm px-2 social-links"><FaGithub /></a>
+                <a href="mailto:abigaildawson.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="btn btn-sm px-2 social-links"><MdEmail /></a>
+                <a href="https://x.com/abigailddev" target="_blank" rel="noopener noreferrer" className="btn btn-sm px-2 social-links"><FaXTwitter /></a>
+              </div>
+              <hr />
+            </div>
           </div>
-        </div>
-      </section>
-    <LandingPageFooter />
+        </section>
+      <LandingPageFooter />
     </div>
     </>
   );
