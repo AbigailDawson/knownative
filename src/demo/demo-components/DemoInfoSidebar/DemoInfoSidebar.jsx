dffa0f8f12e88a1fa50fa074a766737cc6cc4a70
avnms <avnmsarma@gmail.com>
2024-08-27 11:42:34 +1000

    Fix empty space issue between sidebar title and collapse icon for Info

--- src/demo/demo-components/DemoInfoSidebar/DemoInfoSidebar.jsx
+++ src/demo/demo-components/DemoInfoSidebar/DemoInfoSidebar.jsx
@@ -1,15 +1,14 @@
-import "./DemoInfoSidebar.css"
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
+import "./DemoInfoSidebar.css";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
 export default function DemoInfoSidebar({
     handleShowExit,
     handleBackArrowClick,
 }) {
-
     return (
         <div className="info-section">
             <header className="demo-info-header-container">
-                <h1 className="sidebar-heading">Info</h1>
+                <h3 className="sidebar-heading">Info</h3>
                 <ChevronLeftIcon
                     fontSize="large"
                     data-tooltip-id="info-tooltip"
@@ -19,28 +18,32 @@ export default function DemoInfoSidebar({
                 />
             </header>
             <br />
-            <p className="demo-info-paragraph">Thank you for trying the KnowNative demo!</p>
-            <p className="demo-info-paragraph">KnowNative is currently in development. Check out the links below to learn more or get in touch.</p>
-            <button
-                className="demo-info-dark-btn"
-                onClick={null}>
+            <p className="demo-info-paragraph">
+                Thank you for trying the KnowNative demo!
+            </p>
+            <p className="demo-info-paragraph">
+                KnowNative is currently in development. Check out the links
+                below to learn more or get in touch.
+            </p>
+            <button className="demo-info-dark-btn" onClick={null}>
                 Join our mailing list
             </button>
-            <button
-                className="demo-info-light-btn"
-                onClick={null}>
+            <button className="demo-info-light-btn" onClick={null}>
                 Contact us
             </button>
             <button
                 className="demo-info-light-btn"
-                onClick={() => window.open("https://github.com/AbigailDawson/knownative", "_blank")}>
+                onClick={() =>
+                    window.open(
+                        "https://github.com/AbigailDawson/knownative",
+                        "_blank"
+                    )
+                }>
                 Visit GitHub
             </button>
-            <button
-                className="demo-info-light-btn"
-                onClick={handleShowExit}>
+            <button className="demo-info-light-btn" onClick={handleShowExit}>
                 Return to homepage
             </button>
         </div>
-    )
+    );
 }
