2e9f884ec37258b3986620c115c5832b9f9e5764
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-08-29 14:53:55 -0400

    Style how to contribute section
    Signed-off-by: Abigail Dawson <abigaildawson.dev@gmail.com>

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -39,19 +39,24 @@ export default function AboutPage() {
         </div>
       </section>
       <section className="how-to-contribute">
-        <h1 className="about-header">How to contribute</h1>
-        <p className="about-body">KnowNative is a community-driven project that thrives on the ideas, inspiration and expertise from our contributors. We're eager to collaborate with community members skilled in software development, UX/UI design, graphic art and illustration, branding, copywriting and content creation. If you have an interest in linguistics or language study, we invite you to join us in shaping KnowNative's development!</p>
-        <p className="about-body">There are two ways to contribute to the KnowNative project:</p>
-        <ul>
-          <li>
-            <span className="li-header">Contribute to the open source project</span>
-            Software developers are welcome to contribute code to the project! Any kind of contribution is meaningful, whether it's a few lines of CSS to improve some styling or an entirely new feature. To contribute to the open source project, follow the instructions on <a href="https://github.com/AbigailDawson/knownative" target="_blank" rel="noreferrer">GitHub</a> to run KnowNative locally, create your own branch and submit a pull request!
-          </li>
-          <li>
-            <span className="li-header">Become a Core Contributor</span>
-            Core Contributors are involved in the long-term creative vision for the KnowNative project. We work together as a team to plan and implement new features and improve the app experience for our users. Core Contributors commit to participating in regular team meetings and contribute to the project on a regular consistent basis. If you're interested in becoming a Core Contributor, please reach out to Abigail on <a href="https://www.linkedin.com/in/abigaildawsondev/" target="_blank" rel="noreferrer">LinkedIn</a>.
-          </li>
-        </ul>
+        <div className="how-to-contribute-grid">
+          <div className="how-to-contribute-left">
+            <h1>How to contribute</h1>
+            <p className="about-body">KnowNative is a community-driven project that thrives on the ideas, inspiration and expertise from our contributors. We're eager to collaborate with community members skilled in software development, UX/UI design, graphic art and illustration, branding, copywriting and content creation. If you have an interest in linguistics or language study, we invite you to join us in shaping KnowNative's development!</p>
+          </div>
+          <div className="how-to-contribute-right">
+            <ul>
+            <li>
+              <span className="li-header">Contribute to the open source project</span>
+              Software developers are welcome to contribute code to the project! Any kind of contribution is meaningful, whether it's a few lines of CSS to improve some styling or an entirely new feature. To contribute to the open source project, follow the instructions on <a href="https://github.com/AbigailDawson/knownative" target="_blank" rel="noreferrer">GitHub</a> to run KnowNative locally, create your own branch and submit a pull request!
+            </li>
+            <li>
+              <span className="li-header">Become a Core Contributor</span>
+              Core Contributors are involved in the long-term creative vision for the KnowNative project. We work together as a team to plan and implement new features and improve the app experience for our users. Core Contributors commit to participating in regular team meetings and contribute to the project on a regular consistent basis. If you're interested in becoming a Core Contributor, please reach out to Abigail on <a href="https://www.linkedin.com/in/abigaildawsondev/" target="_blank" rel="noreferrer">LinkedIn</a>.
+            </li>
+          </ul>
+          </div>
+        </div>
       </section>
       <section className="backstory" id="backstory">
         <div>
