696b543c53e8768ba80dd76714e32ee4bdc0d1ac
psantos2107 <paul.santos2107@gmail.com>
2024-09-09 10:10:10 -0700

    finish app provider

--- src/contexts/AppProvider.jsx
+++ src/contexts/AppProvider.jsx
@@ -1,1 +1,13 @@
-//will add this later
+import { SavedWordsProvider } from "./SavedWords/SavedWordsProvider";
+import { SidebarProvider } from "./Sidebar/SidebarProvider";
+
+//allows all providers to be in one place
+function AppProvider({ children }) {
+  return (
+    <SavedWordsProvider>
+      <SidebarProvider>{children}</SidebarProvider>
+    </SavedWordsProvider>
+  );
+}
+
+export default AppProvider;
