e1b41a15ebe3cfd407b892ddaf620bc430f9943d
Zephyr <45801099+zephyr-c@users.noreply.github.com>
2024-09-16 12:28:07 -0700

    add SavedWords action enum and skeleton action functions

--- src/contexts/SavedWords/SavedWordsActions.js
+++ src/contexts/SavedWords/SavedWordsActions.js
@@ -1,1 +1,11 @@
-//code to be inputted later
+export const LOAD = "LOAD";
+export const ADD = "ADD";
+export const DELETE = "DELETE";
+export const EDIT = "EDIT";
+export const CLEAR = "CLEAR";
+
+export const actionLoad = () => {};
+export const actionAdd = () => {};
+export const actionDelete = () => {};
+export const actionEdit = () => {};
+export const actionClear = () => {};
