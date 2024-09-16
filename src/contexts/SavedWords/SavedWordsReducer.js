7b93b7aca95cd356451f984ef93f45caebab0856
Zephyr <45801099+zephyr-c@users.noreply.github.com>
2024-09-16 14:27:26 -0700

    create function stubs in SavedWordsReducer

--- src/contexts/SavedWords/SavedWordsReducer.js
+++ src/contexts/SavedWords/SavedWordsReducer.js
@@ -1,5 +1,19 @@
 import { useReducer } from "react";
+import {LOAD, ADD, DELETE, UPDATE, CLEAR} from "SavedWordsActions.js"
+
+const initialState = {
+  savedWords: []
+}
+
+function savedWordsReducer(state, action){
+  switch(action.type){
+    default:
+      throw new Error("invalid action");
+  }
+}
 
 export default function useSavedWordsState() {
-  return "to be coded later";
+  const [state, dispatch] = useReducer(savedWordsReducer, initialState)
+  
+  return {state: state, dispatch: dispatch};
 }
