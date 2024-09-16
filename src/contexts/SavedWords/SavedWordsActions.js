7b6f6427165b2816843f143bbddc2655baa336e8
Zephyr <45801099+zephyr-c@users.noreply.github.com>
2024-09-16 13:52:40 -0700

    fill in savedwords action functions

--- src/contexts/SavedWords/SavedWordsActions.js
+++ src/contexts/SavedWords/SavedWordsActions.js
@@ -1,11 +1,20 @@
 export const LOAD = "LOAD";
 export const ADD = "ADD";
 export const DELETE = "DELETE";
-export const EDIT = "EDIT";
+export const UPDATE = "UPDATE";
 export const CLEAR = "CLEAR";
 
-export const actionLoad = () => {};
-export const actionAdd = () => {};
-export const actionDelete = () => {};
-export const actionEdit = () => {};
-export const actionClear = () => {};
+// load a user's saved words into state
+export const actionLoadWords = (words) => ({type: LOAD, data: words});
+
+// add a new saved word
+export const actionAddWord = (word) => ({type: ADD, data: word});
+
+// delete an existing saved word
+export const actionDeleteWord = (id) => ({type: DELETE, id: id});
+
+// edit an existing saved word
+export const actionUpdateWord = (data) => ({type: UPDATE, data: data});
+
+// delete all saved words
+export const actionClearWords = () => ({type: CLEAR, data: []});
