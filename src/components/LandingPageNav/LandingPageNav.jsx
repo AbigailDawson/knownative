4808c374476194d610ee103efa41c8085514a330
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-09-20 15:04:14 -0400

    Move contact page content to landing page
    Signed-off-by: Abigail Dawson <abigaildawson.dev@gmail.com>

--- src/components/LandingPageNav/LandingPageNav.jsx
+++ src/components/LandingPageNav/LandingPageNav.jsx
@@ -1,56 +1,47 @@
-import { Link } from "react-router-dom";
-import "./LandingPageNav.css";
+import { Link } from 'react-router-dom';
+import './LandingPageNav.css';
 
 export default function LandingPageNav() {
-    return (
-        <nav class="navbar navbar-expand-lg navbar-light mx-4 py-3">
-            <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="/">
-                    <img
-                        src="/images/horizontal-logo.png"
-                        alt="logo"
-                        className="horizontal-logo"
-                    />
-                </a>
-                <button
-                    class="navbar-toggler navbar-toggler-right"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarResponsive"
-                    aria-controls="navbarResponsive"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/about">
-                                About
-                            </Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/contribute">
-                                Contribute
-                            </Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/contact">
-                                Contact
-                            </Link>
-                        </li>
-                        <li class="nav-item">
-                            <a
-                                class="nav-link"
-                                href="https://github.com/AbigailDawson/knownative"
-                                target="_blank"
-                                rel="noreferrer">
-                                GitHub
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light mx-4 py-3">
+      <div className="container px-4 px-lg-5">
+        <a className="navbar-brand" href="/">
+          <img src="/images/horizontal-logo.png" alt="logo" className="horizontal-logo" />
+        </a>
+        <button
+          className="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ms-auto my-2 my-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contribute">
+                Contribute
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="https://github.com/AbigailDawson/knownative"
+                target="_blank"
+                rel="noreferrer">
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
