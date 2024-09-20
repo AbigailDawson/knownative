3063df44febe0c378fd512f41b3a98e0484dbe68
psantos2107 <paul.santos2107@gmail.com>
2024-09-20 00:25:03 -0700

    add comments and slight changes

--- src/contexts/Sidebar/SidebarActions.js
+++ src/contexts/Sidebar/SidebarActions.js
@@ -2,10 +2,13 @@ export const EXPAND = "EXPAND";
 export const CLOSE = "CLOSE";
 export const CHANGE = "CHANGE";
 
-//necessary? They're not really imported anywhere
-export const actionExpandSidebar = () => ({ type: EXPAND, value: true });
-export const actionCloseSidebar = () => ({ type: CLOSE, value: false });
+//Is this necessary? They're not really imported anywhere from what I can see.
+export const actionExpandSidebar = (category) => ({
+  type: EXPAND,
+  payload: category,
+});
+export const actionCloseSidebar = () => ({ type: CLOSE });
 export const actionChangeSidebar = (category) => ({
   type: CHANGE,
-  category: category,
+  payload: category,
 });
--- src/contexts/Sidebar/SidebarReducer.js
+++ src/contexts/Sidebar/SidebarReducer.js
@@ -5,14 +5,15 @@ import { EXPAND, CLOSE, CHANGE } from "./SidebarActions";
 const initialState = { expanded: false, category: null };
 //the actual reducer function
 
+//I chose to just hardcode "true" for expanded in the EXPAND action and "false" for expanded in the CLOSE action because I feel like those actions should always expand or close the sidebar if you call their respective dispatch function. No need to really store it in the payload
 const sidebarReducer = function (state, action) {
   switch (action.type) {
     case EXPAND:
-      return { expanded: true, category: action.data };
+      return { expanded: true, category: action.payload };
     case CLOSE:
       return { expanded: false, category: null };
     case CHANGE:
-      return { ...state, category: action.data };
+      return { ...state, category: action.payload };
     default:
       throw new Error("invalid action");
   }
