cb3a40e3df4e55be635cf359b7fc8a8e1afcb7a3
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-10-05 09:15:23 -0400

    Fix import statement in src/index.jsx
    Signed-off-by: Abigail Dawson <abigaildawson.dev@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './pages/App/App'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,4 +10,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </Router>
   </StrictMode>
-)
+);
