358fc8ee429dc99fa8111d3fae34bcbf6a74acf0
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-10-05 15:07:30 -0400

    Simplify status logs in database
    Signed-off-by: Abigail Dawson <abigaildawson.dev@gmail.com>

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 
@@ -15,27 +15,3 @@ db.on('error', (err) => {
 db.on('disconnected', () => {
   console.log('MongoDB connection disconnected.');
 });
-
-// Function to check connection status
-function checkConnectionStatus() {
-  const status = db.readyState;
-  switch (status) {
-    case 0:
-      console.log('MongoDB is disconnected.');
-      break;
-    case 1:
-      console.log('MongoDB is connected.');
-      break;
-    case 2:
-      console.log('MongoDB is connecting.');
-      break;
-    case 3:
-      console.log('MongoDB is disconnecting.');
-      break;
-    default:
-      console.log('Unknown MongoDB connection status.');
-  }
-}
-
-// Call the function to check connection status
-checkConnectionStatus();
