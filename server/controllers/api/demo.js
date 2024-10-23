88ee0b93b3471c7703227790ef7c2ed56af1fe53
arzansa <arzansa@gmail.com>
2024-10-22 19:41:31 -0500

    add nodejieba

--- server/controllers/api/demo.js
+++ server/controllers/api/demo.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const {Translate} = require('@google-cloud/translate').v2
 const tokenize = require('chinese-tokenizer').loadFile(path.join(__dirname, '../../config/cedict_ts.u8.txt'))
+const tokenize_jieba = require('nodejieba');
 const translate = new Translate({ key: process.env.GOOGLE_TRANSLATE_API_KEY });
 
 module.exports = {
@@ -14,8 +15,11 @@ async function getDemo(req, res) {
 }
 
 function tokenizeText(req, res) {
+  console.log('tokenizeText called')
   const { text } = req.body
-  const tokenizedText = tokenize(text)
+  console.log('text (pre-tokenize):', text)
+  const tokenizedText = tokenize_jieba.cut(text)
+  console.log('text (post-tokenize):', tokenizedText)
   res.json(tokenizedText)
 }
 
