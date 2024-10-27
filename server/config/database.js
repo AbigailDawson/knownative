a8550ca0c1612bd586fde918f36b4f5da584c123
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-10-27 18:52:37 -0400

    Update MongoDB connection to use MONGODB_URI
    Signed-off-by: Abigail Dawson <abigaildawson.dev@gmail.com>

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.MONGODB_URI);
+console.log("Heroku MongoDB URI:", process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 
