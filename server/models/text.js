c8d63f81dc00c1982362de6a6af0ad735fbd3393
vik-wed <wedel.viktoria@gmail.com>
2024-11-30 11:25:04 -0500

    Add note on level to text model

--- server/models/text.js
+++ server/models/text.js
@@ -6,6 +6,8 @@ const textSchema = new Schema(
     title: { type: String, required: true, unique: true, trim: true },
     source: { type: String },
     content: { type: String },
+    // level needs to be added once we can classify a text correctly upon upload
+    // level: { type: String, enum: ["A1", "A2", "B1", "B2", "C1", "C2"] },
     cards: [{ type: Schema.Types.ObjectId, ref: "Card" }],
     tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
     status: {
