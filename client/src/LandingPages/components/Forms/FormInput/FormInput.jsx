b5e8faf967aea489e0ddf06fc60cf00f36af76e7
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-12-03 22:59:51 -0600

    error messages show like they are styled in the wireframes. Though currently, there is a big gap between inputs. Login Page inputs still need error messages

--- client/src/LandingPages/components/Forms/FormInput/FormInput.jsx
+++ client/src/LandingPages/components/Forms/FormInput/FormInput.jsx
@@ -4,13 +4,22 @@ import './FormInput.scss';
 const FormInput = (props) => {
   const { label, htmlFor, onChange, id, errorMessage, ...otherInputProps } = props;
 
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
-    <div className="input-box__input-container">
+    <div className="input-box__input-container has-error">
       <input
         {...otherInputProps}
         id={id}
         onChange={onChange}
         placeholder=" "
+        onBlur={handleFocus}
+        onFocus={() => otherInputProps.name === 'confirmPassword' && setFocused(true)}
+        focused={focused.toString()}
         aria-describedby={`${id}-error`}
         aria-invalid={!!errorMessage}
         className="input-box__input-field"
@@ -19,9 +28,14 @@ const FormInput = (props) => {
         <span className="input-box__label-text">{label}</span>
       </label>
       {otherInputProps.name !== 'password' && (
-        <span id={`${id}-error`} className="input-box__label-text--error" role="alert">
-          {errorMessage}
-        </span>
+        <div id={`${id}-error`} className="input-box__label-text--error" role="alert">
+          <img
+            className="input-box__error-symbol"
+            src="/images/error_note.svg"
+            alt="error symbol"
+          />
+          <span>{errorMessage}</span>
+        </div>
       )}
     </div>
   );
