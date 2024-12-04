e5448653878bc2aa264b6f4d252386bcb538f99d
JP_Escobari <jpe911@gmail.com>
2024-12-04 19:52:09 -0400

    Migrate KnowNativePage to .scss and BEM class naming convention

--- client/src/KnowNativePage/DashboardPage/DashboardPage.jsx
+++ client/src/KnowNativePage/DashboardPage/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import './DashboardPage.css';
+import './DashboardPage.scss';
 import { useEffect } from 'react';
 import { useAuthContext } from '../../contexts/Auth/AuthProvider';
 import { Link, useNavigate } from 'react-router-dom';
@@ -21,138 +21,138 @@ export default function DashboardPage() {
 
   return user ? (
     <div className="dashboard">
-      <div className="dashboard side-nav">
-        <div className="nav-main-container">
-          <a href="../" className="dashboard logo-container">
-            <img src="/images/square-logo.png" alt="KnowNative logo." className="dashboard logo" />
+      <div className="dashboard__side-nav">
+        <div className="dashboard__side-nav-links-container">
+          <a href="../" className="dashboard__logo-container">
+            <img src="/images/square-logo.png" alt="KnowNative logo." className="dashboard__logo" />
             <h4>KnowNative</h4>
           </a>
-          <ul className="nav-links">
-            <li>
-              <a className="link active" href="/dashboard">
+          <ul className="dashboard__nav-links">
+            <li className="dasboard__nav-item">
+              <a className="dashboard__link dashboard__link--active" href="/dashboard">
                 Dashboard
               </a>
             </li>
-            <li>
-              <a className="link" href="/">
+            <li className="dasboard__nav-item">
+              <a className="dashboard__link" href="/">
                 Cards
               </a>
             </li>
-            <li>
-              <a className="link" href="/">
+            <li className="dasboard__nav-item">
+              <a className="dashboard__link" href="/">
                 Library
               </a>
             </li>
-            <li>
-              <a className="link" href="/">
+            <li className="dasboard__nav-item">
+              <a className="dashboard__link" href="/">
                 Progress
               </a>
             </li>
-            <li>
-              <a className="link" href="/">
+            <li className="dasboard__nav-item">
+              <a className="dashboard__link" href="/">
                 Resources
               </a>
             </li>
           </ul>
         </div>
-        <div className="nav-footer-container">
-          <ul className="nav-links">
-            <li>
-              <button className="link" onClick={handleLogOut}>
+        <div className="dashboard__nav-footer">
+          <ul className="dashboard__nav-links">
+            <li className="dasboard__nav-item">
+              <button className="dashboard__link" onClick={handleLogOut}>
                 Logout
               </button>
             </li>
-            <li>
-              <a className="link" href="/">
+            <li className="dasboard__nav-item">
+              <a className="dashboard__link" href="/">
                 GitHub
               </a>
             </li>
-            <li>
-              <a className="link" href="/">
+            <li className="dasboard__nav-item">
+              <a className="dashboard__link" href="/">
                 Contact Us
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <div className="main-container">
-        <div className="user-info">
-          <button>
-            <p>{user.username}</p>
+      <div className="dashboard__main">
+        <div className="dashboard__user-info">
+          <button className='dashboard__user-dropdown-options'>
+            <p className='dashboard__user-name'>{user.username}</p>
             <img
-              className="user-profile-picture"
+              className="dashboard__user-profile-pic"
               src="/images/square-logo.png"
               alt="User profile picture."
             />
-            <p className="user-profile-dropdown">┕</p>
+            <p className="dashboard__user-dropdown-icon">┕</p>
           </button>
         </div>
-        <div className="dashboard header-container">
+        <div className="dashboard__title">
           {/*Just added user.username for testing of the token. Please adjust as needed. */}
           <h1>Dashboard</h1>
         </div>
-        <div className="stats-container">
-          <div className="stat">
-            <p>Level</p>
+        <div className="dashboard__stats-container">
+          <div className="dashboard__stat">
+            <p className='dashboard__stat-label'>Level</p>
             <h2>TOCFL2</h2>
-            <p>Beginner</p>
+            <p className='dashboard__stat-description'>Beginner</p>
           </div>
-          <div className="stat">
-            <p>Texts</p>
+          <div className="dashboard__stat">
+            <p className='dashboard__stat-label'>Texts</p>
             <h2>21</h2>
-            <p className="hidden">Spacing Text</p>
+            <p className="dashboard__stat-description dashboard__stat-description--hidden">Spacing Text</p>
           </div>
-          <div className="stat">
-            <p>Cards</p>
+          <div className="dashboard__stat">
+            <p className='dashboard__stat-label'>Cards</p>
             <h2>154</h2>
-            <p className="hidden">Spacing Text</p>
+            <p className="dashboard__stat-description dashboard__stat-description--hidden">Spacing Text</p>
           </div>
-          <div className="stat">
-            <p>Streak</p>
+          <div className="dashboard__stat">
+            <p className='dashboard__stat-label'>Streak</p>
             <h2>12 Days</h2>
-            <p className="hidden">Spacing Text</p>
+            <p className="dashboard__stat-description dashboard__stat-description--hidden">Spacing Text</p>
           </div>
         </div>
-        <div className="dashboard subtitle">
+        <div className="dashboard__title dashboard__title--subtitle">
           <h4>Jump back in</h4>
         </div>
         <div>
-          <div className="sub-container">
-            <div className="dashboard sub-content">
-              <h2 className="dashboard card-title">開計程車</h2>
-              <p className="dashboard card-body">
+          <div className="dashboard__sub-container">
+            <div className="dashboard dashboard__card">
+              <h2 className="dashboard dashboard__card-title">開計程車</h2>
+              <p className="dashboard dashboard__card-body">
                 每天我要到許多地方去，也會遇到很多人。有些人喜款叫我「左轉」、「右轉」、「停」...
               </p>
             </div>
-            <button>Study</button>
+            <button className='dashboard__study-button'>Study</button>
           </div>
         </div>
-        <div className="dashboard subtitle">
+        <div className="dashboard__title dashboard__title--subtitle">
           <h4>Recently added</h4>
         </div>
-        <div className="recently-added">
-          <div className="sub-container">
-            <div className="dashboard sub-content">
-              <h2 className="dashboard card-title">信用卡雌然很方便</h2>
-              <p className="dashboard card-body">
+        <div className="dashboard__recently-added">
+          <div className="dashboard dashboard__recently-added-content">
+            <div className="dashboard dashboard__card">
+              <h2 className="dashboard dashboard__card-title">信用卡雌然很方便</h2>
+              <p className="dashboard dashboard__card-body">
                 信用卡雏然很方便，但是不小心的话，一下子就會花掉很多绕，所以現在還不了錢的人越来越多。…
               </p>
             </div>
-            <button>Study</button>
+            <button className='dashboard__study-button'>Study</button>
           </div>
-          <div className="sub-container">
-            <div className="dashboard sub-content">
-              <h2 className="dashboard card-title">信用卡雌然很方便</h2>
-              <p className="dashboard card-body">
+          <div className="dashboard__sub-container">
+            <div className="dashboard dashboard__card">
+              <h2 className="dashboard dashboard__card-title">信用卡雌然很方便</h2>
+              <p className="dashboard dashboard__card-body">
                 信用卡雏然很方便，但是不小心的话，一下子就會花掉很多绕，所以現在還不了錢的人越来越多。…
               </p>
             </div>
-            <button>Study</button>
+            <button className='dashboard__study-button'>Study</button>
           </div>
         </div>
-        <div className="dashboard card-buttons">
-          <button className="add-item-button">+ Add text</button>
-          <button className="view-all-button">View all</button>
+        <div className="dashboard dashboard__card-buttons">
+          <button className="dashboard__card-buttons--add-item">+ Add text</button>
+          <button className="dashboard__card-buttons--view-all">View all</button>
         </div>
       </div>
     </div>
