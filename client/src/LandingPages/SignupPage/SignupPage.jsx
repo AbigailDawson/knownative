768be0c66fdd6b3a66a8a9f70d4110675c718626
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-12-10 07:52:53 -0600

    adding FormInput component changes to the Sign up page

--- client/src/LandingPages/SignupPage/SignupPage.jsx
+++ client/src/LandingPages/SignupPage/SignupPage.jsx
@@ -11,16 +11,19 @@ import { RiLockPasswordFill } from 'react-icons/ri';
 import { FaUserPlus } from 'react-icons/fa6';
 import * as authService from '../../services/authService';
 import { useAuthContext } from '../../contexts/Auth/AuthProvider';
+import { validateInput } from '../../utilities/validation';
 
 const SignupPage = () => {
   const [inputValue, setInputValue] = useState({
     firstName: '',
     lastName: '',
+    username: '',
     email: '',
     password: '',
     confirmPassword: ''
   });
 
+  const [inputErrors, setInputErrors] = useState({});
   const [errorMsg, setErrorMsg] = useState('');
   const navigate = useNavigate();
   const { setUser } = useAuthContext();
@@ -33,9 +36,7 @@ const SignupPage = () => {
       type: 'text',
       id: 'signup-firstName',
       htmlFor: 'signup-firstName',
-      errorMessage:
-        "First name needs to be 2 to 20 characters long and shouldn't contain special characters.",
-      pattern: '^[A-Za-z]{2,20}$',
+      // pattern: '^[A-Za-z]{2,20}$',
       // icon: <TbPencilCheck />,
       required: true
     },
@@ -45,9 +46,7 @@ const SignupPage = () => {
       type: 'text',
       id: 'signup-lastName',
       htmlFor: 'signup-lastName',
-      errorMessage:
-        "Last name needs to be 2 to 20 characters long and shouldn't contain special characters.",
-      pattern: '^[A-Za-z]{2,20}$',
+      // pattern: '^[A-Za-z]{2,20}$',
       // icon: <TbPencilCheck />,
       required: true
     },
@@ -57,9 +56,7 @@ const SignupPage = () => {
       type: 'text',
       id: 'signup-username',
       htmlFor: 'signup-username',
-      errorMessage:
-        'Username should be 3-20 characters. Only letters, numbers and underscores are allowed.',
-      pattern: '^[A-Za-z0-9_]{3,20}$',
+      // pattern: '^[A-Za-z0-9_]{3,20}$',
       // icon: <FaUserPlus />,
       required: true
     },
@@ -69,7 +66,6 @@ const SignupPage = () => {
       type: 'email',
       id: 'signup-email',
       htmlFor: 'signup-email',
-      errorMessage: 'Please use a valid email address.',
       // icon: <MdAlternateEmail />,
       required: true
     },
@@ -79,9 +75,7 @@ const SignupPage = () => {
       type: 'password',
       id: 'signup-password',
       htmlFor: 'signup-password',
-      errorMessage:
-        'Password should be 6-20 characters. Include letters, numbers and at least one special character.',
-      pattern: '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,20}$',
+      // pattern: '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,20}$',
       // icon: <RiLockPasswordFill />,
       required: true
     },
@@ -91,7 +85,6 @@ const SignupPage = () => {
       type: 'password',
       id: 'signup-confirmPassword',
       htmlFor: 'signup-confirmPassword',
-      errorMessage: 'Passwords do not match',
       pattern: inputValue.password,
       // icon: <RiLockPasswordFill />,
       required: true
@@ -100,6 +93,17 @@ const SignupPage = () => {
 
   const handleChange = (e) => {
     setInputValue({ ...inputValue, [e.target.name]: e.target.value });
+  };
+
+  const handleBlur = (e) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    // Validate the specific field
+    const newErrors = validateInput({ ...inputValue, [fieldName]: fieldValue });
+
+    // Update the error state only for the specific field
+    setInputErrors({ ...inputErrors, [fieldName]: newErrors[fieldName] });
   };
 
   async function handleSubmit(evt) {
@@ -122,40 +126,44 @@ const SignupPage = () => {
     <main className="signup-page-main">
       <LandingPageNav />
       <section className="signup-page__container">
-          <h1 className="signup-page__title">Create Your Knownative Account</h1>
-          {errorMsg && (
-            <div className="signup-page__error-message">
-              <h5>
-                <em>{errorMsg}</em>
-              </h5>
-            </div>
-          )}
-          <form className="signup-page__form" onSubmit={handleSubmit}>
-            {formFields.map((input, idx) => (
-              <FormInput
-                key={idx}
-                {...input}
-                value={inputValue[input.name]}
-                onChange={handleChange}
-              />
-            ))}
-            <PasswordValidation password={inputValue.password} />
-            <button className='signup-page__button' type="submit">
-              Sign Up
-            </button>
-            {/* oAuth  */}
-            <button className='signup-page__button signup-page__button-google'>
-              <img className='signup-page__button-google--icon' src='../../../public/images/google-icon.png' />
-                Sign up with Google
-            </button>
-            {/* line */}
-            <div>
-              <Link to="/login" className="signup-text">
-                Already have an account? Log in
-              </Link>
-            </div>
-          </form>
-    
+        <h1 className="signup-page__title">Create Your Knownative Account</h1>
+        {errorMsg && (
+          <div className="signup-page__error-message">
+            <h5>
+              <em>{errorMsg}</em>
+            </h5>
+          </div>
+        )}
+        <form className="signup-page__form" onSubmit={handleSubmit}>
+          {formFields.map((input, idx) => (
+            <FormInput
+              key={idx}
+              {...input}
+              value={inputValue[input.name]}
+              onChange={handleChange}
+              errorInputMessage={inputErrors[input.name]}
+              handleBlur={handleBlur}
+            />
+          ))}
+          <PasswordValidation password={inputValue.password} />
+          <button className="signup-page__button" type="submit">
+            Sign Up
+          </button>
+          {/* oAuth  */}
+          <button className="signup-page__button signup-page__button-google">
+            <img
+              className="signup-page__button-google--icon"
+              src="../../../public/images/google-icon.png"
+            />
+            Sign up with Google
+          </button>
+          {/* line */}
+          <div>
+            <Link to="/login" className="signup-text">
+              Already have an account? Log in
+            </Link>
+          </div>
+        </form>
       </section>
     </main>
   );
