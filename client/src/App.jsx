c80b77a6df854bb2a2a131dd5028be97e34020e5
JP_Escobari <jpe911@gmail.com>
2024-12-10 19:40:51 -0400

    Add a Simple Form for Text Imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import DashboardPage from './KnowNativePage/DashboardPage/DashboardPage';
 import AppProvider from './contexts/AppProvider';
 import SignupSuccessful from './KnowNativePage/SignupSuccessful/SignupSuccessful';
 import LoginSuccessful from './KnowNativePage/LoginSuccessful/LoginSuccessful';
+import AddTextPage from './KnowNativePage/AddTextPage/AddTextPage';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login-success" element={<LoginSuccessful />} />
           <Route path="/signup-success" element={<SignupSuccessful />} />
+          <Route path="/add-text" element={<AddTextPage />} />
           <Route path="/*" element={<LandingPage />} />
         </Routes>
       </main>
