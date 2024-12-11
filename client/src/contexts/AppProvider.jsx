edeeae5c23fea834fa291dc002637eacdd4c3cf6
Zephyr <45801099+zephyr-c@users.noreply.github.com>
2024-12-11 14:40:23 -0800

    Moved Sidebar and SavedWords providers and reducers into contexts folder

--- client/src/contexts/AppProvider.jsx
+++ client/src/contexts/AppProvider.jsx
@@ -1,8 +1,17 @@
 import { AuthProvider } from './Auth/AuthProvider';
+import { SavedWordsProvider } from './SavedWords/SavedWordsProvider';
+import { SidebarProvider } from './Sidebar/SidebarProvider';
 
 //allows all providers to be in one place
 function AppProvider({ children }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <SavedWordsProvider>
+        <SidebarProvider>
+          {children}
+        </SidebarProvider>
+      </SavedWordsProvider>
+    </AuthProvider>);
 }
 
 export default AppProvider;
