82b83cce845fc16751b9588756928050df8893fe
abigaildawson <abigaildawson.dev@gmail.com>
2025-02-08 11:59:09 -0500

    Moved favicon and updated path

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 // Configure both serve-favicon & static middleware
 // to serve from the production 'dist' folder
-app.use(favicon(path.join(__dirname, '../client/public/favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 app.use(express.static(path.join(__dirname, '../client/dist')))
 
 // Put API routes here, before the "catch all" route
