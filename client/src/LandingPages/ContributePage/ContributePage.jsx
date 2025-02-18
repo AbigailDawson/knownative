971ff576c82ed2990845db2efce283adb7d82716
abigaildawson <abigaildawson.dev@gmail.com>
2025-02-17 20:46:25 -0500

    Update styling and text copy on featured contributions section and cards
    Signed-off-by: abigaildawson <abigaildawson.dev@gmail.com>

--- client/src/LandingPages/ContributePage/ContributePage.jsx
+++ client/src/LandingPages/ContributePage/ContributePage.jsx
@@ -38,13 +38,9 @@ export default function ContributePage() {
         <LandingPageNav />
         <section className="intro">
           <h1 className="about-header">Contribute to KnowNative</h1>
-          <p className="about-body">There are many ways to get involved with the KnowNative project - see how below!</p>
+          <p className="about-body">KnowNative is a community-driven project that thrives on the ideas, inspiration, and expertise from our contributors. Check out our <a href="https://github.com/AbigailDawson/knownative" target="_blank" >GitHub repository</a> for instructions on how to run the project locally, find an issue & submit your work. All contributors are encouraged to join our <a href="https://discord.gg/Rsc7TAyw3T" target="_blank">Discord server</a> to get in touch with the team, ask questions and share ideas.</p>
         </section>
-        <section className="how-to-contribute">
-          <h1>How to contribute</h1>
-          <p className="about-body">KnowNative is a community-driven project that thrives on the ideas, inspiration, and expertise from our contributors.</p>
-        </section>
-        <section className="contribution-section">
+        <section className="contribution-section"> 
           <ContributionCards />
         </section>
         <section className="github-contributions mt-5">
