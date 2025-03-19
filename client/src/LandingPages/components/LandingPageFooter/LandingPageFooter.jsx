1b5b1419c4c84174224bf6b532b77d3a5bac517c
abigaildawson <abigaildawson.dev@gmail.com>
2025-03-19 15:20:40 -0400

    Update year in footer
    Signed-off-by: abigaildawson <abigaildawson.dev@gmail.com>

--- client/src/LandingPages/components/LandingPageFooter/LandingPageFooter.jsx
+++ client/src/LandingPages/components/LandingPageFooter/LandingPageFooter.jsx
@@ -4,7 +4,7 @@ export default function LandingPageFooter() {
   return (
     <div className="container footer">
       <footer className="py-3 my-4">
-        <p className="text-center text-body-secondary">© 2024 KnowNative</p>
+        <p className="text-center text-body-secondary">© 2025 KnowNative</p>
       </footer>
     </div>
   );
