30ea588fd1e6e9d35e53c182449469efbfad978f
Nishant Movaliya <63806205+nishantmovaliya@users.noreply.github.com>
2025-03-24 21:28:11 +0530

    update: env variable name from SECRET to JWT_SECRET

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 //creates a web token from the JWT library
 function createJWT(user) {
-  return jwt.sign({ user }, process.env.SECRET, { expiresIn: "24h" });
+  return jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "24h" });
 }
 
 //verifies the presence of a JWT
