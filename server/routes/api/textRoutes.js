34d3188cc5805044b06f35182969a08663764a86
tflinch <tflinch16@gmail.com>
2025-09-04 08:46:19 -0400

    feat: add route '/deleteWord/:wordId'

--- server/routes/api/textRoutes.js
+++ server/routes/api/textRoutes.js
@@ -1,10 +1,16 @@
-const express = require("express");
-const { getUserTexts, deleteUserText, saveWord } = require("../../controllers/api/textController");
+const express = require('express');
+const {
+  getUserTexts,
+  deleteUserText,
+  saveWord,
+  deleteWord,
+} = require('../../controllers/api/textController');
 const router = express.Router();
-const { verifyJWT } = require("./../../utils/jwt");
+const { verifyJWT } = require('./../../utils/jwt');
 
-router.get("/getTexts", verifyJWT, getUserTexts);
-router.delete("/:userId/text/:textId", deleteUserText);
-router.post("/saveWord", verifyJWT, saveWord);
+router.get('/getTexts', verifyJWT, getUserTexts);
+router.delete('/:userId/text/:textId', deleteUserText);
+router.post('/saveWord', verifyJWT, saveWord);
+router.delete('/deleteWord/:wordId', verifyJWT, deleteWord);
 
 module.exports = router;
