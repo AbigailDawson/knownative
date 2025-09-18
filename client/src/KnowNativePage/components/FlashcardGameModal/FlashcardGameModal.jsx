4d0311ab39af43f3e33800c205508002bf59bcd4
JP <jpe911@gmail.com>
2025-09-17 22:03:21 -0400

    Create FlashcardGameModal Folder for Component and Stylesheet

---
+++ client/src/KnowNativePage/components/FlashcardGameModal/FlashcardGameModal.jsx
@@ -0,0 +1,61 @@
+import { Button, Dialog, DialogActions, DialogContent } from '@mui/material';
+import { IoMdClose } from 'react-icons/io';
+import './FlashcardGameModal.scss';
+
+export default function FlashcardGameModal({ wordList, selectedFront = 'chinese', showPinyin = true, open, onClose }) {
+    
+    function handleClose() {
+        onClose();
+    }
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            transitionDuration={420}
+            PaperComponent={({ children }) => (
+                <div
+                    style={{
+                        width: '60vmin',
+                        height: '55vmin',
+                        backgroundColor: 'white',
+                        color: 'var(--drk-txt)',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        padding: '1vmin',
+                        borderRadius: '2vmin'
+                    }}>
+                    {children}
+                </div>
+            )}>
+            <DialogActions
+                style={{
+                    alignSelf: 'flex-end',
+                    padding: '0'
+                }}>
+                <Button onClick={handleClose}>
+                    <IoMdClose className="close-icon" />
+                </Button>
+            </DialogActions>
+            <DialogContent
+                style={{
+                    width: '75%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <div style={{ textAlign: 'center' }}>
+                    <h2>Flashcards Placeholder</h2>
+                    <p>Saved Words: {wordList ? wordList.length : 0}</p>
+                    <p>Selected Front: {selectedFront}</p>
+                    <p>Show Pinyin: {showPinyin ? 'Yes' : 'No'}</p>
+                    <p style={{ marginTop: '20px', color: '#666' }}>
+                        Flashcard.jsx component will be implemented here.
+                    </p>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+}
