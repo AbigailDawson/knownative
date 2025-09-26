c196401728bd66220550f00893acd9ff8e2af52e
tflinch <tflinch16@gmail.com>
2025-09-26 08:31:04 -0400

    feat: delete SavedWordsActionsUtil.js to redeuce complexity

--- client/src/contexts/SavedWords/SavedWordsActionsUtil.js
+++
@@ -1,7 +0,0 @@
-import { actionDeleteWord } from './SavedWordsActions';
-import { deleteCard } from '../../utilities/cards-api';
-
-export async function deleteSavedCard(dispatch, cardId) {
-  await deleteCard(cardId);
-  dispatch(actionDeleteWord(cardId));
-}
