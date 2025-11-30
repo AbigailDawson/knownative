92d5f63136086c3f083013303ab82c63c64673fd
JPEscobari <jpe911@gmail.com>
2025-11-30 17:45:04 -0500

    Add StudyTab Tokens Cache

--- client/src/KnowNativePage/TextPage/StudyTab.jsx
+++ client/src/KnowNativePage/TextPage/StudyTab.jsx
@@ -4,7 +4,7 @@ import "./StudyTab.scss";
 import WordPopup from "./components/WordPopup/WordPopup";
 import Spinner from "../../ui-components/Spinner/spinner.jsx";
 
-export default function StudyTab({ text }) {
+export default function StudyTab({ text, tokenCacheRef }) {
   const [tokens, setTokens] = useState([]);
   const [activeWord, setActiveWord] = useState(null);
   const [detectedLanguage, setDetectedLanguage] = useState(null);
@@ -21,26 +21,58 @@ export default function StudyTab({ text }) {
       rect: e.target.getBoundingClientRect(), // store click position for popup placement
     });
   }
-  
-  useEffect(() => {
-    async function fetchTokens() {
-      if (!text?.content) return;
-      setIsLoading(true);
-      try {
-        const { tokens, detectedLanguage } = await getTextTokens(text._id);
-        setDetectedLanguage(detectedLanguage);
-        setTokens(
-          tokens.map((token) => ( { text: token.word, ...token } ))
-        );
+
+  function cacheTokens(textId, tokens, detectedLanguage) {
+    if (!textId || !tokens) return;
+
+    tokenCacheRef.current = {
+      cachedTextId: textId,
+      cachedTokens: tokens,
+      cachedDetectedLanguage: detectedLanguage,
+    };
+  }
+
+  async function fetchTokens() {
+    
+    if (!text?.content) return;
+
+    // Check if the token cache exists.
+    if (tokenCacheRef.current) {
+      
+      const { cachedTextId, cachedTokens, cachedDetectedLanguage } = tokenCacheRef.current;
+      
+      // IF the current text's ._id matches the cachedtextID, 
+      // use the cached tokens and detected language to set state.
+      if (cachedTextId === text._id) {
+        setTokens(cachedTokens.map((token) => ({ text: token.word, ...token })));
+        setDetectedLanguage(cachedDetectedLanguage);
         setIsLoading(false);
-      } catch (err) {
-        console.error("Tokenization failed:", err);
-        setTokens([{ text: text.content }]); // fallback to displaying the full text.
-        setIsLoading(false);
+        return;
       }
     }
+
+    // IF tokens for this text are not cached:
+    // Fetch the tokens and detected language from the database.
+    // Cache the response.
+    setIsLoading(true);
+    try {
+      const { tokens, detectedLanguage } = await getTextTokens(text._id);
+      setDetectedLanguage(detectedLanguage);
+      setTokens(
+        tokens.map((token) => ( { text: token.word, ...token } ))
+      );
+      cacheTokens(text._id, tokens, detectedLanguage);
+    } catch (err) {
+      setTokens([{ text: text.content }]); // fallback to displaying the full text if fetching fails.
+      setDetectedLanguage('');
+    } finally {
+      setIsLoading(false);
+    }
+  }  
+
+  useEffect(() => {
     fetchTokens();
-  }, [text]);
+  }, [text, tokenCacheRef]);
 
   return (
     <section className="study">
