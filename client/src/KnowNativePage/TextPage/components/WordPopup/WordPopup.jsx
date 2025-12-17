db9649f8159dbb1b853e278549645110a5bcbc7e
JP <jpe911@gmail.com>
2025-12-17 10:03:53 -0500

    Update the duplicate check in WordPopup to use the tokenId

--- client/src/KnowNativePage/TextPage/components/WordPopup/WordPopup.jsx
+++ client/src/KnowNativePage/TextPage/components/WordPopup/WordPopup.jsx
@@ -29,7 +29,8 @@ export default function WordPopup({ word, anchorRect, onClose }) {
   // Check if the word is already saved:
   useEffect(() => {
     const isWordSaved = savedWords.some(
-      (savedWord) => savedWord.frontProperties.traditional === word.chars
+      (savedWord) => savedWord.frontProperties.traditional === word.chars &&
+      savedWord.tokenId === word.tokenId
     );
     setSaved(isWordSaved);
   }, [savedWords, word.chars])
